7c29ca6ebd5867c53daea3f6f445b9c06c4acf7a
Thomas Scherz <scherztc@ucmail.uc.edu>
2024-10-14 14:00:39 -0400

    Cleans up documentation.

--- connect_openai.rb
+++
@@ -1,6 +0,0 @@
-require 'openai-client'
-
-Openai::Client.configure do |c|
-  c.access_token    = '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'
-  c.organization_id = 'organization_id' # optional
-end
